783eec108750cf06fdde6836610230297c3b5ae7
Enes Tekin <59219189+enotekin77@users.noreply.github.com>
2021-02-09 12:46:55 +0300

    Room

--- app/src/main/java/com/anushka/tmdbclient/data/model/Artist.kt
+++ app/src/main/java/com/anushka/tmdbclient/data/model/Artist.kt
@@ -1,11 +1,15 @@
 package com.anushka.tmdbclient.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "popular_artists")
 data class Artist(
     val adult: Boolean,
-    val gender: Int,
+
+    @PrimaryKey
     val id: Int,
-    val known_for: List<KnownFor>,
-    val known_for_department: String,
     val name: String,
     val popularity: Double,
     val profile_path: String
--- app/src/main/java/com/anushka/tmdbclient/data/model/Movie.kt
+++ app/src/main/java/com/anushka/tmdbclient/data/model/Movie.kt
@@ -1,6 +1,12 @@
 package com.anushka.tmdbclient.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "popular_movies")
 data class Movie(
+    @PrimaryKey
     val id: Int,
     val overview: String,
     val poster_path: String,
